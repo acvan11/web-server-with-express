90dc87ec7b045d3696ef616f8bf201958a4f1b6e
acvan11 <anvanc37@gmail.com>
2019-05-09 22:00:19 -0700

    customizing the views directory by templates folder

--- src/app.js
+++ src/app.js
@@ -3,9 +3,37 @@ const express = require('express')
 
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+//set the folder templates for views
+const viewsPath = path.join(__dirname,'../templates')
 
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Andy Van'
+	})
+})
+
+app.get('/about', (req, res) =>{
+	res.render('about', {
+		title: 'About Me',
+		name: 'Andy Van'
+	})
+})
+
+app.get('/help', (req, res) =>{
+	res.render('help', {
+		helpText: 'This is some helpful text'
+	})
+})
 
 app.get('/weather', (req, res) => {
 	res.send({
